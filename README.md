63e8cc44073981f108468dc5e6cc0fcbff401e2e
Jun Huh <juan.huh@hotmail.com>
2017-05-06 16:41:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+## HH.Meeting.Web
+
+This is the frontend application for HH.Meeting that is implemented in React/Redux. To get this project running you need to setup the .NET Api locally.
+
+The link to the project is here - [HH.Meeting](https://github.com/mrshawn191/HH.Meeting/tree/master)
+
+## Application Desgin
+
+
+
+## Getting started
+
+To get started, first install all the necessary dependencies.
+```
+> npm install
+```
+
+Run an initial webpack build
+```
+> webpack
+```
+
+Start the development server (changes will now update live in browser)
+```
+> npm run start
+```
+
+To view your project, go to: [http://localhost:3000/](http://localhost:3000/)
+
+## Dependencies used
+- React
+- React Router
+- Redux
+- Redux-Thunk
+- Redux-Promise
+- Redux-Logger
+- Babel
+- Webpack
+- Axion (Http Library)
+- Material UI
+
+## Links
+
+- [Axion](https://www.patreon.com/thenewboston)
+- [Material-UI](http://www.material-ui.com/)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3000
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/rootReducer.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false']
+            }
+        ]
+    },
+    output: {
+        path: 'src',
+        filename: 'js/bundle.min.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+};
